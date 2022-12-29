964713bc3e4836b105b44fb4e10c5ed084d06e72
farzanapomy <farzanapomy56@gmail.com>
2022-12-29 08:25:04 -1200

    devtool-added

--- moon-tech-react-redux/src/redux/store.js
+++ moon-tech-react-redux/src/redux/store.js
@@ -3,5 +3,5 @@ import productReducer from './reducer/productReducer';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
 const store = createStore(productReducer, composeWithDevTools());
-
+//search redux devtools extension on chrome as an extension
 export default store;
