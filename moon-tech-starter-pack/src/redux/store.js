a32a6311121a71aebb91a3eb251adf0a0afb1765
farzanapomy <farzanapomy56@gmail.com>
2023-01-01 00:34:12 +0600

    starter-pack-added

--- moon-tech-starter-pack/src/redux/store.js
+++ moon-tech-starter-pack/src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import productReducer from "./reducers/productReducer";
+import { filterReducer } from "./reducers/filterreducer";
 
 const store = createStore(productReducer, composeWithDevTools());
 
