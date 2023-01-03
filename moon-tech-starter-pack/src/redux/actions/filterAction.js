f86e6e163e4c042642e1a694c5ef8bd84d685190
farzanapomy <farzanapomy56@gmail.com>
2023-01-03 22:15:58 +0600

    update-home

--- moon-tech-starter-pack/src/redux/actions/filterAction.js
+++ moon-tech-starter-pack/src/redux/actions/filterAction.js
@@ -1,4 +1,4 @@
-import { TOGGLE_BRAND } from '../actionTypes/actionTypes';
+import { TOGGLE_BRAND, TOGGLE_STOCK } from '../actionTypes/actionTypes';
 
 export const toggleBrands = (brandName) => {
   return {
@@ -6,3 +6,9 @@ export const toggleBrands = (brandName) => {
     payload: brandName,
   };
 };
+
+export const toggleStock=()=>{
+  return {
+    type: TOGGLE_STOCK
+  }
+}
